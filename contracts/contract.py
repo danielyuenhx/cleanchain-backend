3760a7b81cd5b13008229e0eb7830285dec461b8
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2022-12-02 23:22:20 +0530

    Added basic code to keep track of global variables, and local donations

--- contracts/contract.py
+++ contracts/contract.py
@@ -3,10 +3,24 @@ from pyteal.ast.bytes import Bytes
 from pyteal_helpers import program
 
 def approval():
+    global_cleaned_count = Bytes("Cleaned") #uint64
+    global_cleaning_count = Bytes("Cleaning") # uint64
+
+    local_donation = Bytes("Donation") #uint64
+
     return program.event(
-        init = Seq(),
-        opt_in = Seq(),
-        no_op = Seq()
+        init = Seq(
+            App.globalPut(global_cleaned_count, Int(0)),
+            App.globalPut(global_cleaning_count, Int(0)),
+            Approve()
+        ),
+        opt_in = Seq(
+            App.localPut(Txn.sender(), local_donation, Int(0)),
+            Approve()
+        ),
+        no_op = Seq(
+
+        )
     )
     
 
