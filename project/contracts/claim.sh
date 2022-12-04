ee32066d769e79e9a5ec81652528185a6b627bb7
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2022-12-04 13:17:49 +0530

    Bug fixes

--- project/contracts/claim.sh
+++ project/contracts/claim.sh
@@ -7,4 +7,6 @@ source "$(dirname ${BASH_SOURCE[0]})/config.sh"
 goal app call \
     --app-id "$APP_ID" \
     -f "$CLAIMANT_ACCOUNT" \
-    --app-arg "str:claim"
+    --app-arg "str:claim" \
+    --app-arg "str:40" \
+    --dryrun-dump -o dryrun.msog
