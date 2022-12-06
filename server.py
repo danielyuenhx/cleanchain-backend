cc8ed3e986713bfca48fd03e55eccb0a7cead022
Daniel Yuen <yuendaniel33@gmail.com>
2022-12-07 04:25:43 +0800

    refactored server.py for demonstration

--- server.py
+++ server.py
@@ -1,17 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 import requests
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from geopy.geocoders import Nominatim
 from random import *
+import json
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route("/")
-def home():
-    return "<p>Hello, World!</p>"
 
 @app.route("/getSamplePoints")
 def samplePoints():
@@ -27,30 +24,35 @@ def samplePoints():
 
 @app.route("/getSamplePoint/<location>")
 def samplePoint(location):
-    sample_point_url = "https://environment.data.gov.uk/water-quality/id/sampling-point/" + location
-    sample_point = requests.get(sample_point_url).json()['items'][0]
+    # sample_point_url = "https://environment.data.gov.uk/water-quality/id/sampling-point/" + location
+    # sample_point = requests.get(sample_point_url).json()['items'][0]
+
+    sample_point = sample_points_dict[location]
 
     geolocator = Nominatim(user_agent="geoapiExercises")
     sample_point['location'] = geolocator.reverse(str(sample_point["lat"])+","+str(sample_point["long"])).raw['address']
 
-    sample_point['isOpen'] = isOpens[sample_point['notation']]
-    sample_point['bounty'] = bounties[sample_point['notation']]
-
     return sample_point
 
+@app.route("/closeBounty/<location>", methods=['POST'])
+def closeBounty(location):
+    sample_points_dict[location]['isClaimed'] = 1
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+@app.route("/updateBounty/<location>", methods = ['POST'])
+def updateBounty(location):
+    sample_points_dict[location]["bounty"] += request.get_json()['donation']
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
 if __name__ == '__main__':
-    sample_points_url = "https://environment.data.gov.uk/water-quality/id/sampling-point?samplingPointStatus=open"
+    sample_points_url = "http://environment.data.gov.uk/water-quality/id/sampling-point/AN-BUR140"
     sample_points = requests.get(sample_points_url).json()['items']
-    isOpens = dict()
-    bounties = dict()
 
-    for sample in sample_points:
-        isOpen = randint(0, 1)
-        isOpens[sample['notation']] = isOpen
-        sample['isOpen'] = isOpen
-
-        bounty = randint(500,2000)
-        bounties[sample['notation']] = bounty
-        sample['bounty'] = bounty
+    sample_points_dict = dict()
+    for sample_point in sample_points:
+        sample_point['bounty'] = 0
+        sample_point['isClaimed'] = 0
+        sample_point['isConfirmed'] = 0
+        sample_points_dict[sample_point['notation']] = sample_point
 
     app.run(host='localhost', port=8000, debug=True)
